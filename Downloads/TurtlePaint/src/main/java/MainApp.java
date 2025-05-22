4513c1100cb3fa30f7fe4564b57dd03a3c9c6a42
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2025-05-22 11:56:23 -0700

    added the main menu

--- Downloads/TurtlePaint/src/main/java/MainApp.java
+++ Downloads/TurtlePaint/src/main/java/MainApp.java
@@ -28,6 +28,43 @@ public class MainApp {
         return i;
     }
 
+    public static void add() {
+        System.out.println("ADDING....");
+
+    }
+
+    public static void save() {
+        System.out.println("SAVING....");
+    }
+
+
+    public static void mainMenu() {
+        int choice=-1;
+        while (choice!=0){
+            System.out.println("""
+                1) Add
+                2) Save
+                0) Exit
+                """);
+
+            choice = promptInt("Enter Choice", 1);
+            switch (choice) {
+                case 0:
+                    System.exit(0);
+                    break;
+                case 2:
+                    save();
+                    break;
+                default:
+                    add();
+            }
+
+
+        }
+
+    }
+
+
     public static void main(String[] args) {
         // This starter code to get you familiar with how
         // the TurtleLogo application works
@@ -39,8 +76,9 @@ public class MainApp {
 
         // The world is your canvas
         World world = new World(size, size);
-        Turtle turtle = new Turtle(world, size*-0.5, size*-0.5);
+        Turtle turtle = new Turtle(world, size * -0.5, size * -0.5);
 
+        mainMenu();
 
 
 //       //Xshape
@@ -60,13 +98,13 @@ public class MainApp {
 //        s4.draw();
 
         //Better way to do the above
-        var list = Stream.of(
-                new Square(turtle, -100, 0, "GREEN", 6, 100),
-                new Triangle(turtle, 0, 0, "BLACK", 6, 100),
-                new Circle(turtle, 100, 0, "RED", 6, 100)
-        );
-
-        list.forEach(Shape::draw);
+//        var list = Stream.of(
+//                new Square(turtle, -100, 0, "GREEN", 6, 100),
+//                new Triangle(turtle, 0, 0, "BLACK", 6, 100),
+//                new Circle(turtle, 100, 0, "RED", 6, 100)
+//        );
+//
+//        list.forEach(Shape::draw);
 
 
     }
